7452cd341d887a425d2ce1a1b7c28414a44f21c5
Youki YOSHIOKA <yoshioka.youki@gmail.com>
2023-06-28 17:08:34 +0900

    split code with chapter

---
+++ hello-rust/src/chapter1.rs
@@ -0,0 +1,52 @@
+fn main () {
+    enum_code();
+    trait_code();
+}
+
+fn enum_code() {
+    let objective: Option<i32> = Some(1);
+    match objective {
+        Some(x) if x % 2 == 0 => println!("this number is even: {}", x),
+        Some(x) => println!("this number is odd: {}", x),
+        None => print!("none"),
+    }
+    let mut v = vec![];
+    v.push(1);
+}
+
+trait Animal {
+    fn lifespan(&self) -> u32;
+    fn scientific_name(&self) -> String;
+}
+
+struct Dog;
+impl Animal for Dog {
+    fn lifespan(&self) -> u32 {
+        13
+    }
+    fn scientific_name(&self) -> String {
+        "Canis lupus familiaris".to_string()
+    }
+}
+
+struct Cat;
+impl Animal for Cat {
+    fn lifespan(&self) -> u32 {
+        16
+    }
+    fn scientific_name(&self) -> String {
+        "Felis catus".to_string()
+    }
+}
+
+fn trait_code() {
+    let dog = Dog{};
+    let cat = Cat{};
+    show_animal_data(dog);
+    show_animal_data(cat);
+}
+
+fn show_animal_data<T: Animal>(animal: T) {
+    println!("Lifespan: {}", animal.lifespan());
+    println!("Scientific name: {}", animal.scientific_name());
+}
