a4b54c5323095868f14f91f48f9980031d937352
Youki YOSHIOKA <yoshioka.youki@gmail.com>
2023-07-05 23:00:53 +0900

    chapter3 borrow lifetime

--- hello-rust/src/chapter3.rs
+++ hello-rust/src/chapter3.rs
@@ -6,6 +6,20 @@ fn main() {
     for bird in bird_vec {
         bird.tweet();
     }
+
+    // borrow
+    let important_data = "Hello, world.".to_string();
+
+    calc_data(&important_data);
+    println!("{}", important_data);
+
+    // lifetime
+    let mut x = 5;
+    let _y = &x; // yのライフタイムがここで終了するので、後続にxの可変参照を渡してもエラーにならない
+    let z = &mut x;
+
+    dbg!(z);
+    dbg!(x);
 }
 
 trait Tweet {
@@ -25,4 +39,8 @@ impl Tweet for Duck {
     fn tweet(&self) {
         println!("Quack!");
     }
+}
+
+fn calc_data(data: &String) {
+    println!("{}", data);
 }
